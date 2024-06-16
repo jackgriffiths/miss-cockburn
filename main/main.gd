8f835c459bc47d6b1718121b807c6a86c4a861ed
Jack Griffiths <jack.griffiths@outlook.com>
2024-06-16 17:28:50 +0100

    Fix visibility of the "open menu" button on startup

--- main/main.gd
+++ main/main.gd
@@ -9,6 +9,7 @@ func _ready() -> void:
 	_menu_button.connect("pressed", _menu.show)
 	_menu.connect("visibility_changed", _update_menu_button)
 	_menu.connect("new_game_started", _new_game)
+	_update_menu_button()
 
 
 func _update_menu_button():
