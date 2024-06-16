c197ce4d61b1b9cd6908446e2b0a2f4fbdffe296
Jack Griffiths <jack.griffiths@outlook.com>
2024-06-16 13:55:16 +0100

    Add button to open menu (in addition to ESC key)

--- main/main.gd
+++ main/main.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
 @onready var _table = $Table
+@onready var _menu_button = $UI/Buttons/MenuButton
 @onready var _menu = $Menu
 
 
 func _ready() -> void:
+	_menu_button.connect("pressed", _menu.show)
+	_menu.connect("visibility_changed", _update_menu_button)
 	_menu.connect("new_game_started", _new_game)
+
+
+func _update_menu_button():
+	_menu_button.visible = not _menu.visible
 
 
 func _new_game() -> void:
